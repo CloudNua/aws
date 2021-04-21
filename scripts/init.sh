109aa9905d474d5314ebe096ce68a866bea91541
Enda Kelly <kelly.enda@gmail.com>
2021-04-21 14:05:35 +0000

    fix: ci logic

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,11 +2,11 @@
 set -x
 
 terraform init -reconfigure -upgrade \
-    -backend-config="address=https://gitlab.com/api/v4/projects/<YOUR-PROJECT-ID>/terraform/state/<YOUR-STATE-NAME>" \
-    -backend-config="lock_address=https://gitlab.com/api/v4/projects/<YOUR-PROJECT-ID>/terraform/state/<YOUR-STATE-NAME>/lock" \
-    -backend-config="unlock_address=https://gitlab.com/api/v4/projects/<YOUR-PROJECT-ID>/terraform/state/<YOUR-STATE-NAME>/lock" \
-    -backend-config="username=<YOUR-USERNAME>" \
-    -backend-config="password=<YOUR-ACCESS-TOKEN>" \
+    -backend-config="address=https://gitlab.com/api/v4/projects/26032079/terraform/state/production" \
+    -backend-config="lock_address=https://gitlab.com/api/v4/projects/26032079/terraform/state/production/lock" \
+    -backend-config="unlock_address=https://gitlab.com/api/v4/projects/26032079/terraform/state/production/lock" \
+    -backend-config="username=endk17" \
+    -backend-config="password=$GIT_TOKEN" \
     -backend-config="lock_method=POST" \
     -backend-config="unlock_method=DELETE" \
     -backend-config="retry_wait_min=5"
